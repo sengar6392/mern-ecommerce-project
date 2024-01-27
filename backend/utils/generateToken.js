52986cdf1db616dc40a5938b28267ae28a45b017
Shivam Sengar <shivamsengar995627@gmail.com>
2024-01-27 17:37:44 +0530

    Changed jwt setting

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -8,7 +8,6 @@ const generateToken = (res, userId) => {
   res.cookie('jwt', token, {
     httpOnly: true,
     secure: true, // Use secure cookies in production
-    // sameSite: 'strict', // Prevent CSRF attacks
     maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
   });
 };
